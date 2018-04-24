b4df3121c2ad4e925d77868fb48e56464a10ade9
ahmadaladdasi <ahmadaladdasi88@gmail.com>
2018-04-24 12:01:50 -0700

    seeded the DB with hosted images

--- database/seed.js
+++ database/seed.js
@@ -4,15 +4,19 @@ const faker = require('faker');
 
 let fakeData = [];
 
+
 const randomFakeImageURLs = () => {
   const fakeUrl = [];
   for (let i = 0; i < Math.ceil(Math.random() * 10); i += 1) {
-    fakeUrl.push(faker.image.imageUrl());
+    let num = Math.ceil(Math.random() * 499);
+    num = num.toString().padStart(3, '0');
+    const hostedUrls = `https://s3-us-west-1.amazonaws.com/ahmad-product-images/pic_${num}.jpg`;
+    fakeUrl.push(hostedUrls);
   }
   return fakeUrl;
 };
 
-for (let i = 100000; i < 100100; i += 1) {
+for (let i = 200000; i < 200100; i += 1) {
   fakeData.push({
     id: i,
     name: faker.commerce.productName(),
